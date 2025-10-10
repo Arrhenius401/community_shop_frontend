5ec0669dc053fe6499b2fa08e98e208f7b22238d
Arrhenius401 <1825270596@qq.com>
2025-10-10 17:42:44 +0800

    feat(types): 添加 Vue 自定义全局属性类型声明
    - 声明了 $formatTime 和 $formatePrice 全局方法类型
    - 保留并扩展了 Vue 原有全局属性的类型声明
    - 添加了 vue-router 和 vuex 相关类型导入
    - 完善了 shims-vue.d.ts 文件的类型定义结构

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,26 @@
+// src/shims-vue.d.ts
+// 类型声明文件
+import { ComponentCustomProperties } from 'vue'
+import { Router } from 'vue-router'
+import { Store } from 'vuex'
+
+
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    // 你的自定义全局属性
+    $formatTime: (time: Date) => string
+    $formatePrice: (price: number) => string
+
+    // 确保保留 Vue 原有全局属性的类型声明
+    $router: Router
+    $route: RouteLocationNormalized
+    $store: Store<State> // State 是你的 Vuex 状态类型
+  }
 }
